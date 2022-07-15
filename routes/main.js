192f60803339947e56cd4dde49f001b00d851027
Philip Lanphar <philip.lanphar@gmail.com>
2022-07-15 20:16:29 +0200

    added socket.io

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const server = require('http').createServer(express);
+const io = require('socket.io')(server);
 
 let status = false;
 
 router.post('/set', function (req, res) {
   status = (req.body.status === 'True' || req.body.status === true);
+  io.emit('status', status);
   res.json({status});
 });
 
 router.get('/get', function (req, res) {
   res.json({status});
+});
+
+io.on('connection', (socket) => {
+  console.log('Established a connection!');
+  socket.broadcast.emit('status', status);
+  socket.on('disconnect', () => {
+    console.log('Connection ended!');
+  });
 });
 
 module.exports = router;
