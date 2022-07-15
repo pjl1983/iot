964d417f2af35f6a1c5ab8f68585527942abfe52
Philip Lanphar <philip.lanphar@gmail.com>
2022-07-15 13:30:32 +0200

    updated ui and set initial value

--- public/dist/bundle.js
+++ public/dist/bundle.js
@@ -4400,16 +4400,16 @@ module.exports = {
   },
   setValue(inMeeting) {
     if (inMeeting === true) {
-      document.getElementById('status').innerText = "Go away!";
-      document.getElementById('inMeeting').setAttribute('disabled', '');
-      document.getElementById('notInMeeting').removeAttribute('disabled');
+      document.getElementById('status').innerText = "In a meeting";
+      document.getElementById('toggleSwitch').setAttribute('checked', 'true');
     } else {
-      document.getElementById('status').innerText = "Come on in!";
-      document.getElementById('inMeeting').removeAttribute('disabled');
-      document.getElementById('notInMeeting').setAttribute('disabled', '');
+      document.getElementById('status').innerText = "Not in a meeting";
+      document.getElementById('toggleSwitch').setAttribute('checked', 'false');
     }
   }
 };
+
+module.exports.getStatus();
 
 },{"axios":1}]},{},[37])(37)
 });
